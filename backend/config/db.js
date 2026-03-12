9fcb07769f9be819fe8b1088f80a3f3ebea0c629
Habil <habilmuha14@gmail.com>
2026-03-12 16:59:12 +0700

    feat: setup backend with express, pg connection, and favorites table

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import {Pool} from 'pg';
 const pool =  new Pool({
     user: "postgres",
     host: 'localhost',
-    database: 'absensi_db',
+    database: 'news_aggregator',
     password: 'postgres',
     port: 5432,
 })
